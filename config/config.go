b91caf2a91f61099d3e3b48f10d4043fd694aa5d
thiago.silva <thiago.sdd@live.com>
2023-08-09 16:50:19 -0300

    feat: add sqlite db

--- config/config.go
+++ config/config.go
@@ -1,16 +1,31 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
 var (
-	db     *gorm.DB
-	logger *Logger
+	db *gorm.DB
+	// logger *Logger
 )
 
 func Init() error {
+	var err error
+
+	// init SQLite DB
+	db, err = InitializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("error initialization sqlite: %v", err)
+	}
+
 	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {
