c25e353467c82a39c9045ff3d30ba20dda1c3311
thiago.silva <thiago.sdd@live.com>
2023-08-09 12:05:06 -0300

    feat: add gin-gonic package

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	println("Hello World")
+	// initialize Router
+	r := gin.Default()
+
+	// initialize Routes
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "its working"})
+	})
+
+	// initialize Server
+	r.Run("localhost:8080")
 }
