4752b6b524c6845f457f907415b8ca638b1c9434
thiago.silva <thiago.sdd@live.com>
2023-08-09 15:09:32 -0300

    feat: add logger on config package

---
+++ config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+	return nil
+}
+
+func GetLogger(p string) *Logger {
+	// init logger
+	logger := NewLogger(p)
+
+	return logger
+}
--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "ms-gin-gonic-rest-api-go/router"
+import (
+	"ms-gin-gonic-rest-api-go/config"
+	"ms-gin-gonic-rest-api-go/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	// init Logger
+	logger = config.GetLogger("main")
+
+	// init Config
+	err := config.Init()
+
+	if err != nil {
+		// panic(err)
+		logger.Errorf("config init error: %v", err)
+		return
+	}
+
 	// init Router
 	router.Initialize()
 }
