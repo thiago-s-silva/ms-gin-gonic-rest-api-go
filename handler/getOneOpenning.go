43a72b78949d5425b795164422228fdea20c0346
thiago.silva <thiago.sdd@live.com>
2023-08-10 09:31:14 -0300

    feat: add get one handler

--- handler/getOneOpenning.go
+++ handler/getOneOpenning.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
-	"net/http"
+	"fmt"
+	"ms-gin-gonic-rest-api-go/schemas"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetOneOpenningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Get one openning",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		OnBadRequest(ctx, "invalid query parameter ID")
+		return
+	}
+
+	var openning schemas.Openning
+
+	if err := db.First(&openning, id).Error; err != nil {
+		message := fmt.Sprintf("openning not found for id: %s", id)
+
+		OnNotFound(ctx, message)
+		return
+	}
+
+	OnSuccess(ctx, "message successfuly select query", openning)
 }
