223c30aa4dae6b8007b14e549ce06123a670b0b6
thiago.silva <thiago.sdd@live.com>
2023-08-10 08:47:14 -0300

    feat: add create openning handler

--- handler/createOpenning.go
+++ handler/createOpenning.go
@@ -1,13 +1,36 @@
 package handler
 
 import (
-	"net/http"
+	"ms-gin-gonic-rest-api-go/schemas"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpenningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Create openning",
-	})
+	payload := CreateOpenningRequest{}
+
+	ctx.BindJSON(&payload)
+
+	if err := payload.Validate(); err != nil {
+		OnBadRequest(ctx, err.Error())
+		return
+	}
+
+	openning := schemas.Openning{
+		Role:     payload.Role,
+		Company:  payload.Company,
+		Location: payload.Location,
+		Remote:   *payload.Remote,
+		Link:     payload.Link,
+		Salary:   payload.Salary,
+	}
+
+	dbResult := db.Create(&openning)
+
+	if dbResult.Error != nil {
+		OnError(ctx, "error when trying to create a new openning")
+		return
+	}
+
+	OnCreated(ctx, "openning created", openning)
 }
