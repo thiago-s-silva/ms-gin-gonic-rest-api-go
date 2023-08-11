82ca447d07d47658ed7ac743c2aa4e16198ec587
thiago.silva <thiago.sdd@live.com>
2023-08-11 10:12:14 -0300

    feat: add update handler

--- handler/request.go
+++ handler/request.go
@@ -47,3 +47,21 @@ func (r *CreateOpenningRequest) Validate() error {
 
 	return nil
 }
+
+// UpdateOpenning
+type UpdateOpenningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpenningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+
+	return fmt.Errorf("at least one valid field must be provided")
+}
--- handler/updateOpenning.go
+++ handler/updateOpenning.go
@@ -1,13 +1,64 @@
 package handler
 
 import (
-	"net/http"
+	"fmt"
+	"ms-gin-gonic-rest-api-go/schemas"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateOpenningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Update openning",
-	})
+	var payload UpdateOpenningRequest
+
+	ctx.BindJSON(&payload)
+
+	if err := payload.Validate(); err != nil {
+		OnBadRequest(ctx, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == "" {
+		OnBadRequest(ctx, "invalid query parameter ID")
+		return
+	}
+
+	var openning schemas.Openning
+	if err := db.First(&openning, id).Error; err != nil {
+		message := fmt.Sprintf("openning not found for id: %s", id)
+
+		OnNotFound(ctx, message)
+		return
+	}
+
+	if payload.Role != "" {
+		openning.Role = payload.Role
+	}
+
+	if payload.Company != "" {
+		openning.Company = payload.Company
+	}
+
+	if payload.Location != "" {
+		openning.Location = payload.Location
+	}
+
+	if payload.Remote != nil {
+		openning.Remote = *payload.Remote
+	}
+
+	if payload.Link != "" {
+		openning.Link = payload.Link
+	}
+
+	if payload.Salary > 0 {
+		openning.Salary = payload.Salary
+	}
+
+	if err := db.Save(&openning).Error; err != nil {
+		OnError(ctx, "error when tried to update openning")
+		return
+	}
+
+	OnSuccess(ctx, "successfuly updated", openning)
 }
